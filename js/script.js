7881f44277d5327f15932df110a74559ff2faf1d
shagorpaul001 <136324760+shagorpaul001@users.noreply.github.com>
2023-09-02 13:51:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+//text-change
+function textChange(){
+
+document.getElementById('h1').innerHTML="Hello World";
+
+}
+
+//sit-danger
+function sitDanger(){
+  document.getElementById('h2').innerHTML="Hello work";
+}
+
+//world-change
+function worldChange(){
+
+document.getElementById('h3').innerHTML="World change";
+
+}
+
+//danger world
+function dangerWorld(){
+
+document.getElementById('h4').innerHTML="danger World";
+
+}
+
+//hello bangladesh
+function helloBangladesh(){
+
+document.getElementById('h5').innerHTML="Hello Bangladesh";
+
+}
+
+//hello bangladesh
+function hiBangladesh(){
+
+document.getElementById('h6').innerHTML="Hi Bangladesh";
+
+}
+
+//hello bangladesh
+function hiBangla(){
+
+document.getElementById('h7').innerHTML="Hi Bangla";
+
+}
+
+//hello bangladesh
+function buttonTo(){
+
+document.getElementById('h8').innerHTML="button to";
+
+}
+
+//hello bangladesh
+function helloWorld(){
+
+document.getElementById('h9').innerHTML="Hello World";
+
+}
+
+//hello bangladesh
+function dolorWorld(){
+
+document.getElementById('h10').innerHTML="Dolor World";
+
+}
+
